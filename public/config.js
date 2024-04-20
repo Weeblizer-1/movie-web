38f191672a36ded3ae0ad3ac3018d478bb1cd653
Weeblizer-1 <131916107+Weeblizer-1@users.noreply.github.com>
2024-04-20 11:58:55 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,9 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
-  // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://yes.eliasdavidmiles.workers.dev",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzY5OTM2OWFlZTlhNWEyYzljYjM1Yzk1ZDZjNmViYyIsInN1YiI6IjY1MTRkY2NmOTNiZDY5MDBlMTJjMmQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Krd4b5eW7liKktx8Tri59T_GcGHlQ9kOtNmxE4YAlk",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -12,9 +11,9 @@ window.__CONFIG__ = {
   // Whether to disable hash-based routing, leave this as false if you don't know what this is
   VITE_NORMAL_ROUTER: false,
 
-  // The backend URL to communicate with
+  // The backend URL to communicate with, defaults to the movie-web hosted one at backend.movie-web.app
   VITE_BACKEND_URL: null,
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
-  VITE_DISALLOWED_IDS: "",
+  VITE_DISALLOWED_IDS: ""
 };
